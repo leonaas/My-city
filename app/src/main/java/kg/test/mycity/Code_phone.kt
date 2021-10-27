2d6d15f51f1072f6f6bb5a8b91b0298e308b082a
leonaasanova <asanova_l@auca.kg>
2021-10-27 22:15:16 +0600

    second commit

---
+++ app/src/main/java/kg/test/mycity/Code_phone.kt
@@ -0,0 +1,11 @@
+package kg.test.mycity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Code_phone : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_code_phone)
+    }
+}
