9bb18478c5d5813017f0a721bb6fd57e897f6fc3
leonaasanova <asanova_l@auca.kg>
2021-10-30 22:17:12 +0600

    third commit

---
+++ app/src/main/java/kg/test/mycity/Obrasheniya.kt
@@ -0,0 +1,11 @@
+package kg.test.mycity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Obrasheniya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_obrasheniya)
+    }
+}
---
+++ app/src/main/java/kg/test/mycity/moi_obr.kt
@@ -0,0 +1,11 @@
+package kg.test.mycity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class moi_obr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_moi_obr)
+    }
+}
