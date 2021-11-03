56b0b16fee36b78376ab65f3e4e5334fa2939744
umetalid <umetalid@gmail.com>
2021-11-03 23:39:42 +0600

    commit by darina

--- app/src/main/java/kg/test/mycity/MainActivity_signin.kt
+++ app/src/main/java/kg/test/mycity/MainActivity_signin.kt
@@ -3,9 +3,9 @@ package kg.test.mycity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity_signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`activity_main_login.xml`)
+        setContentView(R.layout.activity_main_signin)
     }
 }
