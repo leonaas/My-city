f01de3ae5fddcd48069498ee718b0ca04f7abc09
leonaasanova <asanova_l@auca.kg>
2021-11-06 23:10:39 +0600

    pochti finish

---
+++ app/src/main/java/kg/test/mycity/menu2_pogoda.kt
@@ -0,0 +1,11 @@
+package kg.test.mycity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu2_pogoda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu2_pogoda)
+    }
+}
