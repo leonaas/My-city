f966f13a71f7dbfe263fdea76d542048d38a2a04
umetalid <umetalid@gmail.com>
2021-11-10 23:33:33 +0600

    color change + logo

---
+++ app/src/main/java/kg/test/mycity/MainActivity_3.kt
@@ -0,0 +1,12 @@
+package kg.test.mycity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        val
+    }
+}
