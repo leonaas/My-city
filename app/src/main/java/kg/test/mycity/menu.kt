92012061f8e0e61ae9faa677cf6c0a68bfbb4c8e
leonaasanova <asanova_l@auca.kg>
2021-11-13 23:48:41 +0600

    final pochti

--- app/src/main/java/kg/test/mycity/menu.kt
+++ app/src/main/java/kg/test/mycity/menu.kt
@@ -1,11 +1,18 @@
 package kg.test.mycity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu2)
+        val CardView = findViewById<CardView>(R.id.obrasheniya)
+
+        CardView.setOnClickListener{
+            startActivity(Intent(this,Obrasheniya::class.java))
+        }
     }
 }
