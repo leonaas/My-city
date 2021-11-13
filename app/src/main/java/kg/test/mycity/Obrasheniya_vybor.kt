7df782d1723843d2a5b84656211fe71ca22a7b4b
madzhunova <madzhunova_a@auca.kg>
2021-11-13 16:54:02 +0600

    final version maybe

---
+++ app/src/main/java/kg/test/mycity/Obrasheniya_vybor.kt
@@ -0,0 +1,11 @@
+package kg.test.mycity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Obrasheniya_vybor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_obrasheniyavybor)
+    }
+}
