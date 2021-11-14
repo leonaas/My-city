bb2ff3a67341876aa30b6b80ab9f25bb93e09778
leonaasanova <asanova_l@auca.kg>
2021-11-15 00:31:04 +0600

    finish pochti

--- app/src/main/java/kg/test/mycity/Obrasheniya_vybor.kt
+++ app/src/main/java/kg/test/mycity/Obrasheniya_vybor.kt
@@ -1,11 +1,20 @@
 package kg.test.mycity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Obrasheniya_vybor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_obrasheniyavybor)
+
+        val ImageButton = findViewById<ImageButton>(R.id.imageView6)
+
+        ImageButton.setOnClickListener{
+            startActivity(Intent(this,menu::class.java))
+            finish()
+        }
     }
 }
